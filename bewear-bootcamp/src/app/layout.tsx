593159cc3398f773fa637362e88dbeba9bf2a91c
Vitoria Goncalves Passos <VitoriaGoncalvesPassos@hotmail.com>
2025-08-21 22:31:16 -0300

    fix: correção warning node

--- bewear-bootcamp/src/app/layout.tsx
+++ bewear-bootcamp/src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
         <ReactQueryProvider>{children}</ReactQueryProvider>
         <Toaster position="top-center" />
